f27f4e1446a3a04de990e7db9e0fcdbbfcd2d1df
Gabriel Mendez <mendezg3@berkeley.edu>
2023-05-09 00:06:26 +0000

    created tools and tests for calculating MAE and ORS2

--- tools/tests/test_utils.py
+++ tools/tests/test_utils.py
@@ -1,10 +1,27 @@
 import numpy as np
+import pandas as pd
+import math
+import statsmodels.formula.api as smf
+from tools import utils
 
-def test_first():
-    assert True
+data_train = {
+  "X": [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+  "Y": [0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
+    }
+df_train = pd.DataFrame(data_train)
+model = smf.ols(formula = 'Y ~ X', data=df_train).fit()
+data_test = {
+  "X": [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+  "Y": [0.0, 1.0, 2.0, 3.5, 4.0, 5.0]
+    }
+df_test = pd.DataFrame(data_test)
 
-def test_second():
-    assert True
-    
-def test_third():
-    assert True
+y_train = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+OSR2 = 0.98
+MAE = 0.083
+
+def test_OSR2():
+    assert math.isclose(utils.OSR2(model, df_test["X"], df_test["Y"], df_train["Y"]), OSR2, abs_tol = 0.01)
+
+def test_MAE():
+    assert math.isclose(utils.MAE(model, df_test["X"], df_test["Y"], df_train["Y"]), MAE, abs_tol = 0.01)
--- tools/utils.py
+++ tools/utils.py
@@ -1,2 +1,11 @@
 import numpy as np
-import pandas as pd
+
+def OSR2(model, X_test, y_test, y_train):
+    y_pred = model.predict(X_test)
+    SSE = np.sum((y_test - y_pred)**2)
+    SST = np.sum((y_test - np.mean(y_train))**2)
+    return 1 - SSE/SST
+
+def MAE(model, X_test, y_test, y_train):
+    y_pred = model.predict(X_test)
+    return sum(abs(y_test - y_pred)) / len(y_test)
